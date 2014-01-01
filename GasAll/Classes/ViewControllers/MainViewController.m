d27ebf107993155d784131363c0295cd27c48633
abalbontin <alvaro@mobivery.com>
2014-01-01 11:51:07 +0100

    Center on user location.

--- GasAll/Classes/ViewControllers/MainViewController.m
+++ GasAll/Classes/ViewControllers/MainViewController.m
@@ -7,10 +7,18 @@
 //
 
 #import "MainViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface MainViewController ()
+@interface MainViewController () <MKMapViewDelegate>
 
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIToolbar *toolbar;
+
+@property (nonatomic) BOOL showUserLocation;
+
+- (IBAction)centerUserLocation:(id)sender;
+
+- (void)setUserLocationRegion;
 
 @end
 
@@ -32,6 +40,9 @@
     
     // We translucent the toolbar with the current color.
     self.toolbar.alpha = 0.9;
+        
+    self.showUserLocation = YES;
+    self.mapView.showsUserLocation = YES;
     
 }
 
@@ -39,6 +50,42 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Class methods
+
+- (IBAction)centerUserLocation:(id)sender {
+    
+    if (self.mapView.userLocation.location.coordinate.latitude != 0.0 && self.mapView.userLocation.location.coordinate.longitude != 0.0) {
+        
+        [self setUserLocationRegion];
+        
+    }
+    
+}
+
+- (void)setUserLocationRegion {
+    
+    MKCoordinateRegion mapRegion;
+    mapRegion.center = self.mapView.userLocation.coordinate;
+    mapRegion.span.latitudeDelta = 0.05;
+    mapRegion.span.longitudeDelta = 0.05;
+    
+    [self.mapView setRegion:mapRegion animated: YES];
+    
+}
+
+#pragma mark - Protocol MKMapViewDelegate methods
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    if (self.showUserLocation) {
+        
+        self.showUserLocation = NO;
+        
+        [self setUserLocationRegion];
+        
+    }
 }
 
 @end
