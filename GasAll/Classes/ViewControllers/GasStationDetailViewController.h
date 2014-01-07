82da4c4ba1f842a91a09b1a69ba5b8d8be0cb680
abalbontin <alvaro@mobivery.com>
2014-01-07 01:06:48 +0100

    Created GasStationDetailViewController class.

---
+++ GasAll/Classes/ViewControllers/GasStationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  GasStationDetailViewController.h
+//  GasAll
+//
+//  Created by Álvaro Balbontín Gutiérrez on 07/01/14.
+//  Copyright (c) 2014 Mobivery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GasStationDetailViewController : UIViewController
+
+@end
---
+++ GasAll/Classes/ViewControllers/GasStationDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  GasStationDetailViewController.m
+//  GasAll
+//
+//  Created by Álvaro Balbontín Gutiérrez on 07/01/14.
+//  Copyright (c) 2014 Mobivery. All rights reserved.
+//
+
+#import "GasStationDetailViewController.h"
+
+@interface GasStationDetailViewController ()
+
+@end
+
+@implementation GasStationDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
